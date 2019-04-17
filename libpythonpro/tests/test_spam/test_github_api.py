1b344922e70980fe7685b419accd26b4277c6439
wherculano <wagherculano@hotmail.com>
2019-04-16 21:48:54 -0400

    Isolando o teste de integração do teste unitário utilizando fixture

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,12 +1,26 @@
 from unittest.mock import Mock
+import pytest
 from PythonPro.libpythonpro import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars2.githubusercontent.com/u/26460999?v=4'
     resp_mock.json.return_value = {'login': 'wherculano', 'id': 26460999,
-                                   'avatar_url': 'https://avatars2.githubusercontent.com/u/26460999?v=4'
+                                   'avatar_url': url
                                    }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('wherculano')
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('wherculano')
     assert 'https://avatars2.githubusercontent.com/u/26460999?v=4' == url
